1dfb6e2047c231cac86af467baa6e96bd64e8c13
errorkimero-eng <errorkimero@gmail.com>
2026-03-22 21:25:27 +0000

    Inicando o super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,35 +5,42 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  char carta[10];
-  char codigo[20], estado[20];
-  char cidade[20];
-  int populacao, pontos, pib;
-  float area;
+  char carta1[10], carta2[10];
+  char codigo1[20], estado1[20], codigo2[20], estado2[20];
+  char cidade1[20], cidade2[20];
+  int populacao1, pontos1, pib1, populacao2, pontos2, pib2;
+  float area1, densidade1, percapita1, area2, densidade2, percapita2;
 
+
+  printf(" **** SUPER TRUNFO ****");
+  prinff("Digite os dados da 1º carta")
   printf("Digite o número da sua carta: \n");
-  scanf("%s", carta);
+  scanf("%s", carta1);
 
   printf("Digite o código da carta: \n");
-  scanf("%s", codigo);
+  scanf("%s", codigo1);
 
   printf("Digite o estado: \n");
-  scanf("%s", estado);
+  scanf("%s", estado1);
 
   printf("Qual o nome da cidade: \n");
-  scanf("%s", cidade);
+  scanf("%s", cidade1);
 
   printf("Qual a população?: \n");
-  scanf("%d", &populacao);
+  scanf("%d", &populacao1);
 
   printf("Qual o PIB: \n");
-  scanf("%d", &pib);
+  scanf("%d", &pib1);
 
   printf("Quantos pontos turisticos existe?: \n");
-  scanf("%d", &pontos);
+  scanf("%d", &pontos1);
 
   printf("A aréa é?: \n");
-  scanf("%f", &area);
+  scanf("%f", &area1);
+
+  printf("Digite a renda percapita?: \n");
+  scanf("%f", &percapita1);
+  
   
   // printando as informações das cartas
   printf("\n");
