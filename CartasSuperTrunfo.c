2decbf97e93057a3a317158a3a3466e0b00b98e9
errorkimero-eng <errorkimero@gmail.com>
2026-03-24 23:19:44 +0000

    Finalizando modo aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,26 +5,24 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  char carta1[10], carta2[10];
   char codigo1[20], estado1[20], codigo2[20], estado2[20];
   char cidade1[20], cidade2[20];
   int populacao1, pontos1, pib1, populacao2, pontos2, pib2;
   float area1, densidade1, percapita1, area2, densidade2, percapita2;
-
+  float densidade1 = populacao1 / area1;
+  float densidade2 = populacao2 / area2;
+  float percapita1 = pib1 / populacao1;
+  float percapita2 = pib2 / populacao2;
 
   printf(" **** SUPER TRUNFO ****\n");
-  printf("Digite os dados da 1º carta");
-  printf("Digite o número da sua carta: \n");
-  scanf("%s", &carta1);
-
-  printf("Digite o código da carta: \n");
-  scanf("%s", &codigo1);
+  printf("Digite o código da 1ª carta: \n");
+  scanf("%c", &codigo1);
 
   printf("Digite o estado: \n");
-  scanf("%s", &estado1);
+  scanf("%c", &estado1);
 
   printf("Qual o nome da cidade: \n");
-  scanf("%s", &cidade1);
+  scanf("%c", &cidade1);
 
   printf("Qual a população?: \n");
   scanf("%d", &populacao1);
@@ -38,21 +36,16 @@ int main() {
   printf("A aréa é?: \n");
   scanf("%f", &area1);
 
-  printf("Digite a renda percapita?: \n");
-  scanf("%f", &percapita1);
-  
-  printf("Digite os dados da 2º carta\n");
-  printf("Digite o número da sua carta: \n");
-  scanf("%s", &carta2);
-
-  printf("Digite o código da carta: \n");
-  scanf("%s", &codigo2);
+    
+  printf(" **************************\n");
+  printf("Digite o código da 2º carta: \n");
+  scanf("%c", &codigo2);
 
   printf("Digite o estado: \n");
-  scanf("%s", &estado2);
+  scanf("%c", &estado2);
 
   printf("Qual o nome da cidade: \n");
-  scanf("%s", &cidade2);
+  scanf("%c", &cidade2);
 
   printf("Qual a população?: \n");
   scanf("%d", &populacao2);
@@ -66,15 +59,21 @@ int main() {
   printf("A aréa é?: \n");
   scanf("%f", &area2);
 
-  printf("Digite a renda percapita?: \n");
-  scanf("%f", &percapita2);
-  
+    
   // printando as informações das cartas
-  printf("\n");printf("A carta escolhida é: \n");
-  printf("Carta: %s Código: %s\n", carta1, codigo1);
-  printf("Estado: %s e cidade: %s\n", estado1, cidade1);
+  printf("-------------------------------------\n");
+  printf("Código da 1ª carta: %s\n", codigo1);
+  printf("Cidade e UF: %s - %s\n", cidade1, estado1);
   printf("PIB: %d | População de: %d | Pontos turisticos de: %d\n", pib1, populacao1, pontos1);
   printf("Com a área de %2.f\n", area1);
+  printf("Com a renda percapta de: %2.f\n", percapita1);
+
+  printf("-------------------------------------\n");
+  printf("Código da 2ª carta: %s\n", codigo2);
+  printf("Cidade e UF: %s - %s\n", cidade2, estado2);
+  printf("PIB: %d | População de: %d | Pontos turisticos de: %d\n", pib2, populacao2, pontos2);
+  printf("Com a área de %2.f\n", area2);
+  printf("Com a renda percapta de: %2.f\n", percapita2);
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
